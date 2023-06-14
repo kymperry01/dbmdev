4c0e1910396fb0e25e1193b6d70751eae5a03021
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2023-06-14 20:38:01 +0930

    Supressing unknown global variables NOTES

---
+++ R/dbmdev-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+#' @name dbmdev
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+utils::globalVariables(c("D", "Date", "H", "H0", "Hn", "Hp", "Hx",
+                         "M", "Station", "T0", "Tn", "Tp", "Tx",
+                         "Y", "avTempoC", "d", "datetime", "dev0",
+                         "dev1", "gen", "obs", "stage", "station",
+                         "sunrise", "sunset", "totDays", "totalDev"))
